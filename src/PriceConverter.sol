a9088eded78e3ce0fcc8fdf135b01f6525d11f1b
Rayyan Uddin <rayyanuddin007@gmail.com>
2024-09-24 23:50:30 +0500

    PriceConverter.sol

--- src/PriceConverter.sol
+++ src/PriceConverter.sol
@@ -4,7 +4,6 @@ pragma solidity ^0.8.27;
 import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
 
 library PriceConverter {
-    // We could make this public, but then we'd have to deploy it
     function getPrice(AggregatorV3Interface priceFeed) internal view returns (uint256) {
         // Sepolia ETH / USD Address
         // https://docs.chain.link/data-feeds/price-feeds/addresses
@@ -13,7 +12,6 @@ library PriceConverter {
         return uint256(answer * 10000000000);
     }
 
-    // 1000000000
     function getConversionRate(uint256 ethAmount, AggregatorV3Interface priceFeed) internal view returns (uint256) {
         uint256 ethPrice = getPrice(priceFeed);
         uint256 ethAmountInUsd = (ethPrice * ethAmount) / 1000000000000000000;
